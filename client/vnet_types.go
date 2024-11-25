d91e298746f2c426ca09e83131e48618eb4f9ee9
Dodai-Dodai <59632014nen@gmail.com>
2024-11-25 16:03:44 +0900

    add vnet client

---
+++ client/vnet_types.go
@@ -0,0 +1,9 @@
+package client
+
+type SDNVnet struct {
+	Vnet      string   `json:"vnet"`                // required field
+	Zone      string   `json:"zone"`                // required field
+	Type      string   `json:"type,omitempty"`      // optional but always set 'vnet'
+	Tag       *int64   `json:"tag,omitempty"`       // optional
+	Vlanaware *IntBool `json:"vlanaware,omitempty"` // optional
+}
